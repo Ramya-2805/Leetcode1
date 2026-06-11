58904db9d2efdc4fd908b0f85e7f202192b21d21
Ramya-2805 <ramyam.24aid@kongu.edu>
2026-06-11 23:06:02 +0530

    Time: 4 ms (39.25%) | Memory: 43.5 MB (25.58%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2==1) return false;
+        char [] arr=s.toCharArray();
+        Stack<Character> st=new Stack<>();
+        for(char ch:arr){
+            if(ch=='{'||ch=='('||ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()) return false;
+                if((st.peek()=='{'&& ch=='}')||(st.peek()=='['&& ch==']')||(st.peek()=='('&& ch==')')){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty()==true;
+    }
+}
