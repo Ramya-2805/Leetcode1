da5948d4342d736fa84c18a38dd835e8b13ca40a
Ramya-2805 <ramyam.24aid@kongu.edu>
2026-06-15 00:24:52 +0530

    Time: 11 ms (90.61%) | Memory: 46.3 MB (64.22%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,32 +1,30 @@
 class Solution {
-    
-        private boolean isVowel(char c){
-            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
-                return true;
-            }
-            else{
-                return false;
+    private boolean isVowel(char c){
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='O'||c=='U'){
+            return true;
+        }
+        else return false;
+    }
+    public int maxVowels(String s, int k) {
+        int n=s.length();
+        int vowel=0;
+        for(int i=0;i<k;i++){
+            if(isVowel(s.charAt(i))){
+                vowel++;
             }
         }
-        public int maxVowels(String s, int k) {
-            int n=s.length();
-            int vowel=0;
-            for(int i=0;i<k;i++){
-                if(isVowel(s.charAt(i))){
-                    vowel++;
-                }
-
+        int maxvowel=vowel;
+        for(int i=k;i<n;i++){
+            if(isVowel(s.charAt(i))){ vowel++;
             }
-            int maxVowel=vowel;
-            for(int i=k;i<n;i++){
-                if(isVowel(s.charAt(i)))
-                vowel++;
-                if(isVowel(s.charAt(i-k)))
+            if(isVowel(s.charAt(i-k))){
                 vowel--;
-                if(vowel>maxVowel){
-                    maxVowel=vowel;
-                }
             }
-            return maxVowel;
+            if(vowel>maxvowel){
+                maxvowel=vowel;
+            }
+        }
+        return maxvowel;
     }
-}
+    
+    }
